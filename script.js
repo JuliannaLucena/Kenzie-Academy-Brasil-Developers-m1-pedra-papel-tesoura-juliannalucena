2711e020a4627a3a4501b5c13f34cf4da97d2dbf
Julianna Lucena <juliannalucena19@gmail.com>
2024-07-09 12:30:10 -0300

    Desenvolvendo o codigo

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-/* Desenvolva seu código abaixo */
+function playRockPaperScissor(player1, player2) {
+    // Define as regras do jogo
+    if (player1 === player2) {
+        return "Empate!";
+    }
+    
+    if (
+        (player1 === "Pedra" && player2 === "Tesoura") ||
+        (player1 === "Tesoura" && player2 === "Papel") ||
+        (player1 === "Papel" && player2 === "Pedra")
+    ) {
+        return "Jogador 1 venceu!";
+    } else {
+        return "Jogador 2 venceu!";
+    }
+}
+
+// Exemplos de uso
+console.log(playRockPaperScissor("Tesoura", "Pedra")); // Jogador 2 venceu!
+console.log(playRockPaperScissor("Pedra", "Tesoura")); // Jogador 1 venceu!
+console.log(playRockPaperScissor("Papel", "Pedra")); // Jogador 1 venceu!
+console.log(playRockPaperScissor("Tesoura", "Papel")); // Jogador 1 venceu!
+console.log(playRockPaperScissor("Papel", "Tesoura")); // Jogador 2 venceu!
+console.log(playRockPaperScissor("Pedra", "Pedra")); // Empate
